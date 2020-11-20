5d70234f07dc9180e27cfe5991f2083fa459ef4e
Jonathan Fritz <fritz@nosc.io>
2020-11-20 13:14:52 +0100

    Inventory Table is now a component

--- src/app/components/inventory-table/inventory-table.component.ts
+++ src/app/components/inventory-table/inventory-table.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Item } from 'src/app/models/Item';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-inventory-table',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryTableComponent implements OnInit {
 
-  constructor() { }
+  /**
+  * Columns displayed
+  */
+  displayedColumns: string[] = ['itemId', 'name', 'description', 'available', 'reservationCount', 'ownership'];
+
+  /**
+   * Datasource
+   */
+  dataSource: MatTableDataSource<Item> = [] as any;
+
+  /**
+   * Flag to reference loading state
+   */
+  loadingCompleted = false;
+
+  @ViewChild('searchInput') searchInput: ElementRef;
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private apiService: ApiService,
+  ) {
+    this.loadData();
+  }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Loads the table with data
+   */
+  loadData(): void {
+    this.apiService.getInventory$().subscribe(items => {
+      console.log(items)
+      this.dataSource = new MatTableDataSource<Item>([...items]);
+      this.loadingCompleted = true;
+    });
   }
 
 }
--- src/app/pages/inventory-page/inventory-page.component.ts
+++ src/app/pages/inventory-page/inventory-page.component.ts
@@ -1,9 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { Item } from 'src/app/models/Item';
-import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-inventory-page',
@@ -12,43 +7,11 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class InventoryPageComponent implements OnInit {
 
-  /**
-   * Columns displayed
-   */
-  displayedColumns: string[] = ['itemId', 'name', 'description', 'available', 'reservationCount', 'ownership'];
-
-  /**
-   * Datasource
-   */
-  dataSource: MatTableDataSource<Item> = [] as any;
-
-  /**
-   * Flag to reference loading state
-   */
-  loadingCompleted = false;
-
-  @ViewChild('searchInput') searchInput: ElementRef;
-
-  @ViewChild(MatSort) sort: MatSort;
-
 
   constructor(
-    private apiService: ApiService,
   ) {
-    this.loadData();
   }
 
   ngOnInit(): void {
-  }
-
-  /**
-   * Loads the table with data
-   */
-  loadData(): void {
-    this.apiService.getInventory$().subscribe(items => {
-      console.log(items)
-      this.dataSource = new MatTableDataSource<Item>([...items]);
-      this.loadingCompleted = true;
-    });
   }
 }
